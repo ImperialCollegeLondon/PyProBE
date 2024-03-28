23087f9ad175430b70fe34897718c13d1a731ddb
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2024-03-28 16:21:10 +0000

    Add docstrings to procedure

--- pybatdata/procedure.py
+++ pybatdata/procedure.py
@@ -1,15 +1,37 @@
-import numpy as np
-import pandas as pd
+"""A module for the Procedure class."""
 from pybatdata.experiment import Experiment, Pulsing
 import polars as pl
 from pybatdata.base import Base
 
 class Procedure(Base):
-    def __init__(self, lazyframe, titles, cycles_idx, steps_idx, step_names):
+    """A class for a procedure in a battery experiment."""
+    def __init__(self, 
+                 lazyframe: pl.LazyFrame, 
+                 titles: dict, 
+                 cycles_idx: list, 
+                 steps_idx: list, 
+                 step_names: list):
+        """ Create a procedure class.
+        
+        Args:
+            lazyframe (polars.LazyFrame): The lazyframe of data being filtered.
+            titles (dict): The titles of the experiments inside a procedure. Fomat {title: experiment type}.
+            cycles_idx (list): The indices of the cycles in the current selection.
+            steps_idx (list): The indices of the steps in the current selection.
+            step_names (list): The names of all of the steps in the procedure.
+        """
         super().__init__(lazyframe, cycles_idx, steps_idx, step_names)
         self.titles = titles
         
-    def experiment(self, experiment_name):
+    def experiment(self, experiment_name: str)->Experiment:
+        """Return an experiment object from the procedure.
+        
+        Args:
+            experiment_name (str): The name of the experiment.
+            
+        Returns:
+            Experiment: An experiment object from the procedure.
+        """
         experiment_number = list(self.titles.keys()).index(experiment_name)
         cycles_idx = self.cycles_idx[experiment_number]
         steps_idx = self.steps_idx[experiment_number]
