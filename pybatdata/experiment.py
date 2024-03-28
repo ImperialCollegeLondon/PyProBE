5a2010746ebf8799e8bd9b56be53cc0af72466ee
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2024-03-28 16:31:44 +0000

    Create experiment docstrings

--- pybatdata/experiment.py
+++ pybatdata/experiment.py
@@ -1,12 +1,35 @@
+"""A module for the Experiment class."""
+
 from pybatdata.cycle import Cycle
 import polars as pl
 from pybatdata.base import Base
 
 class Experiment(Base):
-    def __init__(self, lazyframe, cycles_idx, steps_idx, step_names):
+    """A class for an experiment in a battery procedure."""
+    def __init__(self, 
+                 lazyframe: pl.LazyFrame, 
+                 cycles_idx: list, 
+                 steps_idx: list, 
+                 step_names: list):
+        """Create an experiment class.
+
+            Args:
+                lazyframe (polars.LazyFrame): The lazyframe of data being filtered.
+                cycles_idx (list): The indices of the cycles in the current selection.
+                steps_idx (list): The indices of the steps in the current selection.
+                step_names (list): The names of all of the steps in the procedure.
+        """
         super().__init__(lazyframe, cycles_idx, steps_idx, step_names)
 
-    def cycle(self,cycle_number):
+    def cycle(self,cycle_number: int) -> Cycle:
+        """Return a cycle object from the experiment.
+        
+        Args:
+            cycle_number (int): The cycle number to return.
+            
+        Returns:
+            Cycle: A cycle object from the experiment.
+        """
         cycles_idx = self.cycles_idx[cycle_number]
         steps_idx = self.steps_idx[cycle_number]
         conditions = [self.get_conditions('Cycle', cycles_idx),
