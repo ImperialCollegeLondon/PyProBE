7c29249b719af54ad470cd2c03abb6d51c259ec3
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2024-04-01 12:33:37 +0100

    Switch to plotly

--- pybatdata/dashboard.py
+++ pybatdata/dashboard.py
@@ -1,5 +1,3 @@
-#%%
-
 import streamlit as st
 import os
 import pandas as pd
@@ -8,6 +6,9 @@ import sys
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.io import show
+from pygwalker.api.streamlit import StreamlitRenderer, init_streamlit_comm
+import plotly.graph_objects as go
+
 import pandas as pd
 # Add the parent directory of pybatdata to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -16,39 +17,28 @@ with open('procedure_dict.pkl', 'rb') as f:
     procedure_dict = pickle.load(f)
 
 metadata = pd.DataFrame(procedure_dict).drop('Data', axis=1)
+def dataframe_with_selections(df):
+    df_with_selections = df.copy()
+    df_with_selections.insert(0, "Select", False)
+
+    # Get dataframe row-selections from user with st.data_editor
+    edited_df = st.sidebar.data_editor(
+        df_with_selections,
+        hide_index=True,  # Keep the index visible
+        column_config={"Select": st.column_config.CheckboxColumn(required=True)},
+        disabled=df.columns,
+    )
+
+    # Filter the dataframe using the temporary column, then drop the column
+    selected_rows = edited_df[edited_df.Select]
+    selected_indices = selected_rows.index.tolist()  # Get the indices of the selected rows
+    return selected_indices
+
 # Display the DataFrame in the sidebar
-st.sidebar.write(metadata)
-
-# Create a selectbox for each row in the DataFrame
-options = [f'Row {i}' for i in range(len(metadata))]
-selected_row = st.selectbox('Select a row', options)
-
-# Convert the selected row label back to an index
-selected_index = int(selected_row.split(' ')[1])
-
-experiment_names = procedure_dict[selected_index]['Data'].titles.keys()
-selected_experiment = st.selectbox('Select an experiment', experiment_names)
-data = procedure_dict[selected_index]['Data'].experiment(selected_experiment)
-
+selected_indices = dataframe_with_selections(metadata)
 
 # Get the cycle and step numbers from the user
 cycle_step_input = st.text_input('Enter the cycle and step numbers (e.g., "cycle(1).step(2)")')
-
-# Check if the input is not empty
-if cycle_step_input:
-    # Use eval to evaluate the input as Python code
-    try:
-        data_to_plot = eval(f'data.{cycle_step_input}')
-    except Exception as e:
-        st.write(f'Error: {e}')
-else: 
-    data_to_plot = data
-    
-
-
-# Get the column names
-columns = data_to_plot.RawData.to_pandas().columns.tolist()
-
 x_options = ['Time', 'Capacity (Ah)']
 y_options = ['Voltage (V)', 'Current (A)', 'Capacity (Ah)']
 
@@ -56,33 +46,37 @@ y_options = ['Voltage (V)', 'Current (A)', 'Capacity (Ah)']
 x_axis = st.selectbox('Select x axis', x_options, index=0)
 y_axis = st.selectbox('Select y axis', y_options, index=1)
 
-# Create a line chart with the selected axes
-# Convert the data to a pandas DataFrame
-df = data_to_plot.RawData.to_pandas()
-
-# Create a ColumnDataSource from the DataFrame
-source = ColumnDataSource(df)
-
 # Create a figure
 p = figure(title='Bokeh Plot', x_axis_label=x_axis, y_axis_label=y_axis)
+experiment_names = procedure_dict[0]['Data'].titles.keys()
+selected_experiment = st.selectbox('Select an experiment', experiment_names)
 
-# Add a line plot to the figure
-line = p.line(x=x_axis, y=y_axis, source=source)
-# Create a hover tool
-# Create a hover tool
-hover = HoverTool(tooltips=[
-    ("Time", "@Time"),
-    ("Voltage (V)", "@{Voltage (V)}"),
-    ("Current (A)", "@{Current (A)}"),
-    ("Capacity (Ah)", "@{Capacity (Ah)}")
-], renderers=[line])
+selected_names = [procedure_dict[i]['Name'] for i in selected_indices]
+# Create a figure
+fig = go.Figure()
+for selected_index in selected_indices:
+    data = procedure_dict[selected_index]['Data'].experiment(selected_experiment)
+    # Check if the input is not empty
+    if cycle_step_input:
+        # Use eval to evaluate the input as Python code
+        try:
+            data_to_plot = eval(f'data.{cycle_step_input}')
+        except Exception as e:
+            st.write(f'Error: {e}')
+    else: 
+        data_to_plot = data
 
-# Add the hover tool to the figure
-p.add_tools(hover)
+    df = data_to_plot.RawData.to_pandas()
+
+
+    # Add a line to the plot for each selected index
+    fig.add_trace(go.Scatter(x=df[x_axis], y=df[y_axis], mode='lines', name=f'Line {selected_index}'))
+
+# Set the plot's title and labels
+fig.update_layout(title='Plotly Plot', xaxis_title=x_axis, yaxis_title=y_axis, template='simple_white')
+
 # Show the plot
-st.bokeh_chart(p)
+st.plotly_chart(fig)    
 
-
-st.write(data_to_plot.RawData.to_pandas())
-    #%%
+tabs = st.tabs(selected_names)
 
