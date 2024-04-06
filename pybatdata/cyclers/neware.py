acfdde6f0fe99f3bf2eb4867da3c50e2f417cb44
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2024-04-06 18:56:38 +0100

    Remove all possible pandas dependencies

--- pybatdata/cyclers/neware.py
+++ pybatdata/cyclers/neware.py
@@ -1,57 +1,58 @@
-import pandas as pd
-import numpy as np
 import os
 import re
-from pybatdata.batterycycler import BatteryCycler
+import polars as pl
 
-class Neware(BatteryCycler): 
-    @classmethod
-    def load_file(cls, filepath):
+class Neware:
+    @staticmethod
+    def load_file(filepath):
         file_ext = os.path.splitext(filepath)[1]
         if file_ext == '.xlsx':
-            df = pd.read_excel(filepath)
+            df = pl.read_excel(filepath, engine='calamine')
         elif file_ext == '.csv':
-            df = pd.read_csv(filepath)
-        column_dict = {'Date': 'Date', 
-                       'Cycle Index': 'Cycle', 
-                       'Step Index': 'Step', 
-                       'Current(A)': 'Current (A)', 
-                       'Voltage(V)': 'Voltage (V)', 
-                       'DChg. Cap.(Ah)': 'Discharge Capacity (Ah)', 
+            df = pl.read_csv(filepath)
+        column_dict = {'Date': 'Date',
+                       'Cycle Index': 'Cycle',
+                       'Step Index': 'Step',
+                       'Current(A)': 'Current (A)',
+                       'Voltage(V)': 'Voltage (V)',
+                       'DChg. Cap.(Ah)': 'Discharge Capacity (Ah)',
                        'Chg. Cap.(Ah)': 'Charge Capacity (Ah)',
                        }
-        df = cls.convert_units(df)
-        df = df[list(column_dict.keys())].rename(columns=column_dict)
-        dQ_charge = np.diff(df['Charge Capacity (Ah)'])
-        dQ_discharge = np.diff(df['Discharge Capacity (Ah)'])
-        dQ_charge[dQ_charge < 0] = 0
-        dQ_discharge[dQ_discharge < 0] = 0
-        dQ_charge = np.append(0, dQ_charge)
-        dQ_discharge = np.append(0, dQ_discharge)
-        df['Capacity (Ah)'] = np.cumsum(dQ_charge - dQ_discharge)
-        df['Capacity (Ah)'] = df['Capacity (Ah)'] + df['Charge Capacity (Ah)'].max()
-        df['Date'] = pd.to_datetime(df['Date'])
-        df['Time (s)'] = (df['Date'] - df['Date'].iloc[0]).dt.total_seconds()
-        df = df[['Date',
-                 'Time (s)',
-                 'Cycle',
-                 'Step',
-                 'Current (A)',
-                 'Voltage (V)',
-                 'Capacity (Ah)',
-                 ]]
+        df = Neware.convert_units(df)
+        df = df.select(list(column_dict.keys())).rename(column_dict)
+        dQ_charge = df['Charge Capacity (Ah)'].diff(null_behavior="drop")
+        dQ_discharge = df['Discharge Capacity (Ah)'].diff(null_behavior="drop")
+        dQ_charge = dQ_charge.clip(lower_bound=0)
+        dQ_discharge = dQ_discharge.clip(lower_bound=0)
+        dQ_charge = pl.concat([pl.Series([0], dtype=pl.Float64), dQ_charge])
+        dQ_discharge = pl.concat([pl.Series([0], dtype=pl.Float64), dQ_discharge])
+        max_charge_capacity = df['Charge Capacity (Ah)'].max()
+        capacity = (dQ_charge - dQ_discharge).cum_sum() + max_charge_capacity
+        df = df.with_columns(pl.Series('Capacity (Ah)', capacity))
+        # df = df.with_columns(pl.col('Date').str.to_datetime().alias('Date'))
+        df = df.with_columns(pl.col('Date').dt.timestamp('ms').alias('Time (s)'))
+        df = df.with_columns(pl.col('Time (s)') - pl.col('Time (s)').first())
+        df = df.with_columns(pl.col('Time (s)')*1e-3)
+        df = df.select(['Date',
+                       'Time (s)',
+                       'Cycle',
+                       'Step',
+                       'Current (A)',
+                       'Voltage (V)',
+                       'Capacity (Ah)',
+                       ])
         return df
-    
+
     @staticmethod
     def convert_units(df):
         conversion_dict = {'m': 1e-3, 'µ': 1e-6, 'n': 1e-9, 'p': 1e-12}
         for column in df.columns:
-            match = re.search(r'\((.*?)\)', column)  # Update the regular expression pattern to extract the unit from the column name
+            match = re.search(r'\((.*?)\)', column)
             if match:
                 unit = match.group(1)
                 prefix = next((x for x in unit if not x.isupper()), None)
                 if prefix in conversion_dict:
-                    df[column] = df[column] * conversion_dict[prefix]
-                    df.rename(columns={column: column.replace('('+unit+')', '('+unit.replace(prefix, '')+')')}, inplace=True)  # Update the logic for replacing the unit in the column name
-
+                    df = df.with_columns((pl.col(column) * conversion_dict[prefix]).alias(column.replace('('+unit+')', '('+unit.replace(prefix, '')+')')))
+                    #df = df.with_columns(column, df[column] * conversion_dict[prefix])
+                    #df = df.rename(column, column.replace('('+unit+')', '('+unit.replace(prefix, '')+')'))
         return df
--- tests/test_cell.py
+++ tests/test_cell.py
@@ -1,7 +1,7 @@
 import pytest
 from pybatdata.cell import Cell
 from pybatdata.cyclers.neware import Neware
-import pandas as pd
+import polars as pl
 from polars.testing import assert_frame_equal
 
 @pytest.fixture
@@ -17,7 +17,7 @@ def test_read_record(metadata_fixture):
     root_directory = 'tests/sample_data_neware'
     record_name = 'sample_data_neware'
     record = Cell.read_record(root_directory, record_name)
-    pd.testing.assert_frame_equal(record, pd.DataFrame([metadata_fixture]))
+    pl.testing.assert_frame_equal(record, pl.DataFrame([metadata_fixture]))
 
 @pytest.fixture
 def filename_function():
@@ -38,6 +38,6 @@ def test_add_data(cell_instance, procedure_fixture):
     cell_instance.add_data(input_path, title, cycler, skip_writing)
     assert_frame_equal(cell_instance.raw_data[title].RawData, procedure_fixture.RawData)
 
-# def test_verify_parquet():
-#     input_path = 'tests/sample_data_neware/sample_data_neware.xlsx'
-#     assert Cell.verify_parquet(input_path, Neware) == True
+def test_verify_parquet():
+    input_path = 'tests/sample_data_neware/sample_data_neware.xlsx'
+    assert Cell.verify_parquet(input_path, Neware) == True
