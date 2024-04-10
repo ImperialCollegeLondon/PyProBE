ae0878ace77d2625962a0e95220df6047d08bcd9
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2024-04-10 11:33:00 +0100

    Create plotly default plot

--- pybatdata/viewer.py
+++ pybatdata/viewer.py
@@ -3,6 +3,7 @@ import polars as pl
 import plotly.graph_objects as go
 from plotly.express.colors import sample_colorscale
 from sklearn.preprocessing import minmax_scale
+import numpy as np
 class Viewer:
     def __init__(self, data, info):
         self._data = data
@@ -24,11 +25,58 @@ class Viewer:
     def print(self):
         print(self.data)
 
-    def plotly(self, fig, x, y, color_by=None, legend_by='Name', colormap='viridis'):
+    def plotly(self, 
+               fig, 
+               x, 
+               y, 
+               color_by=None, 
+               legend_by='Name', 
+               colormap='viridis'):
         if color_by is not None:
-            unique_colors = self.data[color_by].unique().to_numpy()
-            colors = sample_colorscale(colormap, minmax_scale(unique_colors))
-            for i, condition in enumerate(unique_colors):
-                subset = self.data.filter(pl.col(color_by) == condition)
-                fig.add_trace(go.Scatter(x=subset[x], y=subset[y], mode='lines', line=dict(color=colors[i]), name=self.info[legend_by]))
+            if color_by in self.data.columns:
+                unique_colors = self.data[color_by].unique().to_numpy()
+                colors = sample_colorscale(colormap, minmax_scale(unique_colors))
+                for i, condition in enumerate(unique_colors):
+                    subset = self.data.filter(pl.col(color_by) == condition)
+                    fig.add_trace(go.Scatter(x=subset[x], 
+                                             y=subset[y], 
+                                             mode='lines', 
+                                             line=dict(color=colors[i]), 
+                                             name=str(unique_colors[i]),
+                                             showlegend=False
+                                             ))
+                    
+                # Dummy heatmap for colorbar
+                fig.add_trace(
+                    go.Heatmap(
+                        z=[[unique_colors.min(), unique_colors.max()]],  # Set z to the range of the color scale
+                        colorscale=colormap,  # choose a colorscale
+                        colorbar=dict(title=color_by),  # add colorbar
+                        opacity=0,
+                    )
+                )
+            else:
+                color = self.info['color']
+                fig.add_trace(go.Scatter(x=subset[x], y=subset[y], mode='lines', line=dict(color=color), name=self.info[legend_by]))
+                fig.update_layout(showlegend=True,
+                                  legend = dict(font = dict(size=axis_font_size)))
+        title_font_size = 18
+        axis_font_size = 14
+        plot_theme = 'simple_white'
+        x_range = [self.data[x].min(), self.data[x].max()]
+        y_range = [self.data[y].min(), self.data[y].max()]
+        x_buffer = 0.05 * (x_range[1] - x_range[0])
+        y_buffer = 0.05 * (y_range[1] - y_range[0])
+        fig.update_xaxes(range=[x_range[0] - x_buffer, x_range[1] + x_buffer])
+        fig.update_yaxes(range=[y_range[0] - y_buffer, y_range[1] + y_buffer])
+        fig.update_layout(xaxis_title=x, 
+                  yaxis_title=y,
+                  template=plot_theme,
+                  title_font=dict(size=title_font_size),
+                  xaxis_title_font=dict(size=title_font_size),
+                  yaxis_title_font=dict(size=title_font_size),
+                  xaxis_tickfont=dict(size=axis_font_size),
+                  yaxis_tickfont=dict(size=axis_font_size),
+
+                    )
         return fig
