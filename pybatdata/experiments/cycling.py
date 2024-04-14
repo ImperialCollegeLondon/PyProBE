f3a0ba4b21f8a39532c26ba5d8da1e415f45b152
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2024-04-14 16:43:36 +0100

    mypy fixes for cycling

--- pybatdata/experiments/cycling.py
+++ pybatdata/experiments/cycling.py
@@ -1,47 +1,81 @@
 """A module for the Cycing class."""
 
-from pybatdata.experiment import Experiment
-from pybatdata.step import Step
+from typing import Dict
+
 import polars as pl
-import numpy as np
-from pybatdata.result import Result
+
+from pybatdata.experiment import Experiment
 from pybatdata.filter import Filter
+from pybatdata.result import Result
+
 
 class Cycling(Experiment):
     """A cycling experiment in a battery procedure."""
 
-    def __init__(self, lazyframe, info):
-        """Create a cycling experiment.
+    def __init__(self, lazyframe: pl.LazyFrame, info: Dict[str, str | int | float]):
+        """Create an experiment.
 
-            Args:
-                lazyframe (polars.LazyFrame): The lazyframe of data being filtered.
+        Args:
+            lazyframe (polars.LazyFrame): The lazyframe of data being filtered.
+            info (Dict[str, str | int | float]): A dict containing test info.
         """
         super().__init__(lazyframe, info)
         self._create_capacity_throughput()
 
-    def _create_capacity_throughput(self)->None:
-        """Recalculate the capacity column to show the total capacity passed at each point."""
-        self.lazyframe = self.lazyframe.with_columns([
-            (pl.col("Capacity [Ah]").diff().abs().cum_sum()).alias("Capacity Throughput [Ah]")
-        ])
+    def _create_capacity_throughput(self) -> None:
+        """Calculate the capcity throughput of the experiment."""
+        self.lazyframe = self.lazyframe.with_columns(
+            [
+                (pl.col("Capacity [Ah]").diff().abs().cum_sum()).alias(
+                    "Capacity Throughput [Ah]"
+                )
+            ]
+        )
 
     @property
-    def SOH_capacity(self) -> np.ndarray:
+    def SOH_capacity(self) -> Result:
         """Calculate the state of health of the battery.
 
         Returns:
-            np.ndarray: The state of health of the battery.
+            Result: A result object for the capacity SOH of the cell.
         """
         self.lazyframe = Filter._get_events(self.lazyframe)
-        lf_capacity_throughput = self.lazyframe.groupby('_cycle', maintain_order = True).agg(pl.col('Capacity Throughput [Ah]').first())
-        lf_time = self.lazyframe.groupby('_cycle', maintain_order = True).agg(pl.col('Time [s]').first())
-        
-        lf_charge = self.charge().lazyframe.groupby('_cycle', maintain_order = True).agg(pl.col('Capacity [Ah]').max()-pl.col('Capacity [Ah]').min()).rename({'Capacity [Ah]': 'Charge Capacity [Ah]'})
-        lf_discharge = self.discharge().lazyframe.groupby('_cycle', maintain_order = True).agg(pl.col('Capacity [Ah]').max()-pl.col('Capacity [Ah]').min()).rename({'Capacity [Ah]': 'Discharge Capacity [Ah]'})
-        
-        lf = lf_capacity_throughput.join(lf_time, on='_cycle', how = 'outer_coalesce').join(lf_charge, on='_cycle', how='outer_coalesce').join(lf_discharge, on='_cycle', how='outer_coalesce')
-        
-        lf = lf.with_columns((pl.col('Charge Capacity [Ah]')/pl.first('Charge Capacity [Ah]')*100).alias('SOH Charge (%)'))
-        lf = lf.with_columns((pl.col('Discharge Capacity [Ah]')/pl.first('Discharge Capacity [Ah]')*100).alias('SOH Discharge (%)'))
+        lf_capacity_throughput = self.lazyframe.groupby(
+            "_cycle", maintain_order=True
+        ).agg(pl.col("Capacity Throughput [Ah]").first())
+        lf_time = self.lazyframe.groupby("_cycle", maintain_order=True).agg(
+            pl.col("Time [s]").first()
+        )
+
+        lf_charge = (
+            self.charge()
+            .lazyframe.groupby("_cycle", maintain_order=True)
+            .agg(pl.col("Capacity [Ah]").max() - pl.col("Capacity [Ah]").min())
+            .rename({"Capacity [Ah]": "Charge Capacity [Ah]"})
+        )
+        lf_discharge = (
+            self.discharge()
+            .lazyframe.groupby("_cycle", maintain_order=True)
+            .agg(pl.col("Capacity [Ah]").max() - pl.col("Capacity [Ah]").min())
+            .rename({"Capacity [Ah]": "Discharge Capacity [Ah]"})
+        )
+        lf = (
+            lf_capacity_throughput.join(lf_time, on="_cycle", how="outer_coalesce")
+            .join(lf_charge, on="_cycle", how="outer_coalesce")
+            .join(lf_discharge, on="_cycle", how="outer_coalesce")
+        )
+
+        lf = lf.with_columns(
+            (
+                pl.col("Charge Capacity [Ah]") / pl.first("Charge Capacity [Ah]") * 100
+            ).alias("SOH Charge (%)")
+        )
+        lf = lf.with_columns(
+            (
+                pl.col("Discharge Capacity [Ah]")
+                / pl.first("Discharge Capacity [Ah]")
+                * 100
+            ).alias("SOH Discharge (%)")
+        )
         print(lf.collect())
         return Result(lf, self.info)
