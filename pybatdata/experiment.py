188cd19630b2974edf2a94a29d75749e58d0b1d2
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2024-04-14 16:34:44 +0100

    mypy fixes for experiment

--- pybatdata/experiment.py
+++ pybatdata/experiment.py
@@ -1,30 +1,33 @@
 """A module for the Experiment class."""
 
-from pybatdata.cycle import Cycle
+from typing import Dict
+
 import polars as pl
-from pybatdata.result import Result
+
+from pybatdata.cycle import Cycle
 from pybatdata.filter import Filter
 
+
 class Experiment(Cycle):
-    """ An experiment in a battery procedure."""
-    def __init__(self, 
-                 lazyframe: pl.LazyFrame,
-                 info: dict):
+    """An experiment in a battery procedure."""
+
+    def __init__(self, lazyframe: pl.LazyFrame, info: Dict[str, str | int | float]):
         """Create an experiment.
 
-            Args:
-                lazyframe (polars.LazyFrame): The lazyframe of data being filtered.
+        Args:
+            lazyframe (polars.LazyFrame): The lazyframe of data being filtered.
+            info (Dict[str, str | int | float]): A dict containing test info.
         """
         super().__init__(lazyframe, info)
 
-    def cycle(self,cycle_number: int) -> Cycle:
+    def cycle(self, cycle_number: int) -> Cycle:
         """Return a cycle object from the experiment.
-        
+
         Args:
             cycle_number (int): The cycle number to return.
-            
+
         Returns:
             Cycle: A cycle object from the experiment.
         """
-        lf_filtered = Filter.filter_numerical(self.lazyframe, '_cycle', cycle_number)
+        lf_filtered = Filter.filter_numerical(self.lazyframe, "_cycle", cycle_number)
         return Cycle(lf_filtered, self.info)
