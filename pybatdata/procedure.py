747bc40d751f86cd8b059b4e81b252ceb15eece8
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2024-04-30 09:52:08 +0100

    Remove old readme processing code

--- pybatdata/procedure.py
+++ pybatdata/procedure.py
@@ -1,6 +1,5 @@
 """A module for the Procedure class."""
 import os
-import re
 from typing import Any, Dict, List
 
 import polars as pl
@@ -85,70 +84,6 @@ class Procedure(Result):
             pl.Expr: The polars expression for filtering the column.
         """
         return pl.col(column).is_in(cls.flatten(indices)).alias(column)
-
-    @staticmethod
-    def process_txt(
-        readme_path: str,
-    ) -> tuple[Dict[str, str], List[List[int]], List[List[list[int]]]]:
-        """Function to process the README.txt file and extract the relevant information.
-
-        Args:
-            readme_path (str): The path to the README.txt file.
-
-        Returns:
-            dict: The titles of the experiments inside a procddure.
-                Fomat {title: experiment type}.
-            list: The cycle numbers inside the procedure.
-            list: The step numbers inside the procedure.
-        """
-        with open(readme_path, "r") as file:
-            lines = file.readlines()
-
-        titles = {}
-        title_index = 0
-        for line in lines:
-            if line.startswith("##"):
-                splitted_line = line[3:].split(":")
-                titles[splitted_line[0].strip()] = splitted_line[1].strip()
-
-        steps: List[List[List[int]]] = [[[]] for _ in range(len(titles))]
-        cycles: List[List[int]] = [[] for _ in range(len(titles))]
-        line_index = 0
-        title_index = -1
-        cycle_index = 0
-        while line_index < len(lines):
-            if lines[line_index].startswith("##"):
-                title_index += 1
-                cycle_index = 0
-            if lines[line_index].startswith("#-"):
-                match = re.search(r"Step (\d+)", lines[line_index])
-                if match is not None:
-                    steps[title_index][cycle_index].append(
-                        int(match.group(1))
-                    )  # Append step number to the corresponding title's list
-                    latest_step = int(match.group(1))
-            if lines[line_index].startswith("#x"):
-                line_index += 1
-                match = re.search(r"Starting step: (\d+)", lines[line_index])
-                if match is not None:
-                    starting_step = int(match.group(1))
-                line_index += 1
-                match = re.search(r"Cycle count: (\d+)", lines[line_index])
-                if match is not None:
-                    cycle_count = int(match.group(1))
-                for i in range(cycle_count - 1):
-                    steps[title_index].append(
-                        list(range(starting_step, latest_step + 1))
-                    )
-                    cycle_index += 1
-            line_index += 1
-
-        cycles = [list(range(len(sublist))) for sublist in steps]
-        for i in range(len(cycles) - 1):
-            cycles[i + 1] = [item + cycles[i][-1] for item in cycles[i + 1]]
-        for i in range(len(cycles)):
-            cycles[i] = [item + 1 for item in cycles[i]]
-        return titles, cycles, steps
 
     @staticmethod
     def process_readme(
