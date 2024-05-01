62b539126a257e508f403743d639493f6e97c411
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2024-05-01 10:10:47 +0100

    Create method modules for ICA

---
+++ pybatdata/methods/differentiation/feng_2020.py
@@ -0,0 +1,58 @@
+"""Module for the Feng et al. (2020) method for ICA."""
+from typing import Dict, List
+
+import numpy as np
+import polars as pl
+from numpy.typing import NDArray
+
+from pybatdata.result import Result
+from pybatdata.step import Step
+
+
+def IC(step: Step, parameter_dict: Dict[str, float]) -> Result:
+    """Calculate the normalised incremental capacity of the step.
+
+    Method from: 10.1016/j.etran.2020.100051
+
+    Args:
+        step (Step): A step object.
+        parameter_dict (Dict[str, float]): A dictionary containing
+            the parameters for the method
+
+    Returns:
+        Result: a result object containing the normalised incremental capacity
+    """
+    V = step.data["Voltage [V]"]
+    deltaV = parameter_dict["deltaV"]
+    n = len(V)
+    V_range = V.max() - V.min()
+    v = np.linspace(V.min(), V.max(), int(V_range / deltaV))
+    deltaV = v[1] - v[0]
+
+    N, _ = np.histogram(V, bins=v)
+    IC = N / n * 1 / deltaV
+    v_midpoints = v[:-1] + np.diff(v) / 2
+
+    IC = smooth_IC(IC, [0.0668, 0.2417, 0.3830, 0.2417, 0.0668])
+    result = pl.DataFrame({"Voltage [V]": v_midpoints, "IC [Ah/V]": IC})
+    return Result(result, step.info)
+
+
+def smooth_IC(IC: NDArray[np.float64], alpha: List[float]) -> NDArray[np.float64]:
+    """Smooth the incremental capacity.
+
+    Args:
+        IC (NDArray[np.float64]): The incremental capacity vector.
+        alpha (list[float]): The smoothing coefficients.
+
+    Returns:
+        NDArray[float]: The smoothed incremental capacity.
+    """
+    A = np.zeros((len(IC), len(IC)))
+    w = np.floor(len(alpha) / 2)
+    for n in range(len(alpha)):
+        k = n - w
+        vector = np.ones(int(len(IC) - abs(k)))
+        diag = np.diag(vector, int(k))
+        A += alpha[n] * diag
+    return A @ IC
--- pybatdata/step.py
+++ pybatdata/step.py
@@ -1,11 +1,10 @@
 """A module for the Step class."""
 
-from typing import Dict, List
+from typing import Dict
 
-import numpy as np
 import polars as pl
-from numpy.typing import NDArray
 
+from pybatdata.methods.differentiation import feng_2020
 from pybatdata.result import Result
 
 
@@ -32,49 +31,16 @@ class Step(Result):
         """
         return abs(self.data["Capacity [Ah]"].max() - self.data["Capacity [Ah]"].min())
 
-    def IC(self, deltaV: float) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
+    def dQdV(self, method: str, parameter_dict: Dict[str, float]) -> Result:
         """Calculate the normalised incremental capacity of the step.
 
-        Method from: 10.1016/j.etran.2020.100051
-
         Args:
-            deltaV (float): The voltage sampling interval.
+            method (str): The method to use to calculate the incremental capacity.
+            parameter_dict (Dict[str, float]): A dictionary containing
+                the parameters for the method
 
         Returns:
-            tuple[NDArray[float], NDArray[float]]:
-                The midpints of the voltage sampling intervals and the
-                normalised incremental capacity.
+            Result: a result object containing the normalised incremental capacity
         """
-        V = self.data["Voltage [V]"]
-
-        n = len(V)
-        V_range = V.max() - V.min()
-        v = np.linspace(V.min(), V.max(), int(V_range / deltaV))
-        deltaV = v[1] - v[0]
-
-        N, _ = np.histogram(V, bins=v)
-        IC = N / n * 1 / deltaV
-        v_midpoints = v[:-1] + np.diff(v) / 2
-
-        IC = self.smooth_IC(IC, [0.0668, 0.2417, 0.3830, 0.2417, 0.0668])
-        return v_midpoints, IC
-
-    @staticmethod
-    def smooth_IC(IC: NDArray[np.float64], alpha: List[float]) -> NDArray[np.float64]:
-        """Smooth the incremental capacity.
-
-        Args:
-            IC (NDArray[np.float64]): The incremental capacity vector.
-            alpha (list[float]): The smoothing coefficients.
-
-        Returns:
-            NDArray[float]: The smoothed incremental capacity.
-        """
-        A = np.zeros((len(IC), len(IC)))
-        w = np.floor(len(alpha) / 2)
-        for n in range(len(alpha)):
-            k = n - w
-            vector = np.ones(int(len(IC) - abs(k)))
-            diag = np.diag(vector, int(k))
-            A += alpha[n] * diag
-        return A @ IC
+        method_dict = {"feng_2020": feng_2020.IC}
+        return method_dict[method](self, parameter_dict)
