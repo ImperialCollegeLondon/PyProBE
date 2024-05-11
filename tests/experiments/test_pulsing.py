dde97f867dd37dbcea42019397b50746a198bd9b
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2024-05-11 17:12:55 +0100

    Move cycle and step tests into filter

--- tests/experiments/test_pulsing.py
+++ tests/experiments/test_pulsing.py
@@ -1,31 +1,40 @@
+"""Tests for the pulsing experiment."""
+
 import numpy as np
-# def test_cycle(BreakinCycles_fixture):
-#     cycle = BreakinCycles_fixture.cycle(0)
-#     assert cycle.cycles_idx == 1
-#     assert cycle.steps_idx == [4, 5, 6, 7]
 
 
 def test_pulse(Pulsing_fixture):
+    """Test the pulse method."""
     pulse = Pulsing_fixture.pulse(0)
-    assert (pulse.data['Step'] == 10).all()
-    assert (pulse.data['Cycle'] == 5).all()
-    
+    assert (pulse.data["Step"] == 10).all()
+    assert (pulse.data["Cycle"] == 5).all()
+
+
 def test_V0(Pulsing_fixture):
+    """Test the V0 attribute."""
     assert Pulsing_fixture.V0[0] == 4.1919
     assert len(Pulsing_fixture.V0) == 10
-    
+
+
 def test_V1(Pulsing_fixture):
+    """Test the V1 attribute."""
     assert Pulsing_fixture.V1[0] == 4.1558
     assert len(Pulsing_fixture.V1) == 10
-    
+
+
 def test_I1(Pulsing_fixture):
+    """Test the I1 attribute."""
     assert Pulsing_fixture.I1[0] == -0.0199936
     assert len(Pulsing_fixture.I1) == 10
-    
+
+
 def test_R0(Pulsing_fixture):
-    assert np.isclose(Pulsing_fixture.R0[0], (4.1558-4.1919)/-0.0199936)
+    """Test the R0 attribute."""
+    assert np.isclose(Pulsing_fixture.R0[0], (4.1558 - 4.1919) / -0.0199936)
     assert len(Pulsing_fixture.R0) == 10
-    
+
+
 def test_Rt(Pulsing_fixture):
-    assert np.isclose(Pulsing_fixture.Rt(10)[0], (4.1337-4.1919)/-0.0199936)
+    """Test the Rt method."""
+    assert np.isclose(Pulsing_fixture.Rt(10)[0], (4.1337 - 4.1919) / -0.0199936)
     assert len(Pulsing_fixture.Rt(10)) == 10
