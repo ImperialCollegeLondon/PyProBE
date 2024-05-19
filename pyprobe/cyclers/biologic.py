eba656f42cc232741c665381bc6d86b8b582ea20
tomjholland <thomas.holland7@outlook.com>
2024-05-19 17:22:37 +0100

    Biologic import and test

--- pyprobe/cyclers/biologic.py
+++ pyprobe/cyclers/biologic.py
@@ -2,8 +2,12 @@
 
 
 import os
+from datetime import datetime
 
 import polars as pl
+
+from pyprobe.cyclers.basecycler import BaseCycler
+from pyprobe.unitconverter import UnitConverter
 
 
 def read_file(filepath: str) -> pl.DataFrame:
@@ -15,14 +19,88 @@ def read_file(filepath: str) -> pl.DataFrame:
     filename = os.path.basename(filepath)
     file_ext = os.path.splitext(filename)[1]
 
-    with open(filepath, "r", encoding="shift_jis") as file:
+    with open(filepath, "r", encoding="iso-8859-1") as file:
         file.readline()  # Skip the first line
         second_line = file.readline().strip()  # Read the second line
     _, value = second_line.split(":")
     n_header_lines = int(value.strip())
 
+    with open(filepath, "r", encoding="iso-8859-1") as file:
+        for i in range(n_header_lines):
+            line = file.readline()
+            if "Acquisition started on" in line:
+                start_time_line = line
+                break
+    _, value = start_time_line.split(" : ")
+    start_time = datetime.strptime(value.strip(), "%m/%d/%Y %H:%M:%S.%f")
+
     match file_ext:
         case ".mpt":
-            return pl.read_csv(filepath, skip_rows=n_header_lines - 1, separator="\t")
+            dataframe = pl.read_csv(
+                filepath, skip_rows=n_header_lines - 1, separator="\t"
+            )
         case _:
             raise ValueError(f"Unsupported file extension: {file_ext}")
+
+    time_col = dataframe.select(pl.col("time/s")).to_numpy()
+    start_epoch = start_time.timestamp()
+    time_col = time_col + start_epoch
+    start = pl.DataFrame({"start": [start_time]})
+    dataframe = dataframe.with_columns(
+        (pl.col("time/s") + start).cast(pl.Datetime).alias("Date")
+    )
+    return dataframe
+
+
+def process_dataframe(dataframe: pl.DataFrame) -> pl.DataFrame:
+    """Process a DataFrame from battery cycler data.
+
+    Args:
+        dataframe: The DataFrame to process.
+
+    Returns:
+        pl.DataFrame: The dataframe in PyProBE format.
+    """
+    columns = dataframe.columns
+    time = pl.col("time/s").alias("Time [s]")
+
+    # Cycle and step
+    cycle = pl.col("cycle number").alias("Cycle")
+    step = pl.col("Ns").alias("Step")
+
+    # Measured data
+    column_name_pattern = r"(.+)/(.+)"
+    current = UnitConverter.search_columns(
+        columns, "I", column_name_pattern, "Current"
+    ).to_default()
+    voltage = UnitConverter.search_columns(
+        columns, "Ecell", column_name_pattern, "Voltage"
+    ).to_default()
+
+    make_charge_capacity = UnitConverter.search_columns(
+        columns, "Q charge", column_name_pattern, "Capacity"
+    ).to_default(keep_name=True)
+    make_discharge_capacity = UnitConverter.search_columns(
+        columns, "Q discharge", column_name_pattern, "Capacity"
+    ).to_default(keep_name=True)
+
+    dataframe = dataframe.with_columns(time, cycle, step, current, voltage)
+
+    dataframe = dataframe.with_columns(make_charge_capacity, make_discharge_capacity)
+
+    diff_charge_capacity = (
+        pl.col("Q charge [Ah]").diff().clip(lower_bound=0).fill_null(strategy="zero")
+    )
+    diff_discharge_capacity = (
+        pl.col("Q discharge [Ah]").diff().clip(lower_bound=0).fill_null(strategy="zero")
+    )
+    make_capacity = (
+        (diff_charge_capacity - diff_discharge_capacity).cum_sum()
+        + pl.col("Q charge [Ah]").max()
+    ).alias("Capacity [Ah]")
+
+    dataframe = dataframe.with_columns(make_capacity)
+    return dataframe
+
+
+biologic = BaseCycler(reader=read_file, processor=process_dataframe)
--- tests/cyclers/test_biologic.py
+++ tests/cyclers/test_biologic.py
@@ -2,14 +2,41 @@
 
 import polars as pl
 
-from pyprobe.cyclers.biologic import read_file
+from pyprobe.cyclers.biologic import process_dataframe, read_file
 
 
 def test_read_file():
     """Test the read_file method."""
-    unprocessed_dataframe = read_file(
-        "tests/sample_data_biologic/interim_sample_data_biologic.mpt"
-    )
-    print(unprocessed_dataframe)
+    unprocessed_dataframe = read_file("tests/sample_data_biologic/interim.mpt")
     assert isinstance(unprocessed_dataframe, pl.DataFrame)
-    assert 0 == 1
+
+
+def test_process_dataframe():
+    """Test the Biologic method."""
+    dataframe = pl.DataFrame(
+        {
+            "time/s": [0.0, 1.0, 2.0, 3.0],
+            "cycle number": [1, 1, 1, 1],
+            "Ns": [1, 2, 3, 4],
+            "I/mA": [1, 2, 3, 4],
+            "Ecell/V": [4, 5, 6, 7],
+            "Q charge/mAh": [0, 20, 0, 0],
+            "Q discharge/mAh": [0, 0, 10, 20],
+        }
+    )
+    processed_dataframe = process_dataframe(dataframe)
+    processed_dataframe = processed_dataframe.select(
+        ["Time [s]", "Cycle", "Step", "Current [A]", "Voltage [V]", "Capacity [Ah]"]
+    )
+    expected_dataframe = pl.DataFrame(
+        {
+            "Time [s]": [0.0, 1.0, 2.0, 3.0],
+            "Cycle": [1, 1, 1, 1],
+            "Step": [1, 2, 3, 4],
+            "Current [A]": [1e-3, 2e-3, 3e-3, 4e-3],
+            "Voltage [V]": [4, 5, 6, 7],
+            "Capacity [Ah]": [0.020, 0.040, 0.030, 0.020],
+        }
+    )
+
+    pl.testing.assert_frame_equal(processed_dataframe, expected_dataframe)
