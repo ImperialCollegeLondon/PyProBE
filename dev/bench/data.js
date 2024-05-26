020fd81db8d2629193e423af87fff2cfc699c676
github-action-benchmark <github@users.noreply.github.com>
2024-05-26 11:56:09 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 14cf3ae2e055cd240306b3f4bf84d8d9c350bebc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716720628937,
+  "lastUpdate": 1716724569238,
   "repoUrl": "https://github.com/ImperialCollegeLondon/PyProBE",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -136,6 +136,142 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00010400843951990623",
             "extra": "mean: 592.0668951308924 usec\nrounds: 1335"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "137503955+tomjholland@users.noreply.github.com",
+            "name": "Tom Holland",
+            "username": "tomjholland"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "14cf3ae2e055cd240306b3f4bf84d8d9c350bebc",
+          "message": "Merge pull request #31 from ImperialCollegeLondon/25-make-results-callable\n\nMake result objects callable",
+          "timestamp": "2024-05-26T12:53:00+01:00",
+          "tree_id": "fa37df9530ac01a92628a490ad28fcf6e94e9e32",
+          "url": "https://github.com/ImperialCollegeLondon/PyProBE/commit/14cf3ae2e055cd240306b3f4bf84d8d9c350bebc"
+        },
+        "date": 1716724568508,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/cyclers/test_biologic.py::test_read_and_process",
+            "value": 182.86102375796654,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0004725208386118459",
+            "extra": "mean: 5.4686339354831155 msec\nrounds: 155"
+          },
+          {
+            "name": "tests/cyclers/test_neware.py::test_read_and_process",
+            "value": 0.19230697606035996,
+            "unit": "iter/sec",
+            "range": "stddev: 0.022112695479638896",
+            "extra": "mean: 5.2000193674 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/test_cell.py::test_add_procedure",
+            "value": 504.8735790823153,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00014986915476627324",
+            "extra": "mean: 1.9806938636354323 msec\nrounds: 418"
+          },
+          {
+            "name": "tests/test_filter.py::test_step",
+            "value": 10.336365838580253,
+            "unit": "iter/sec",
+            "range": "stddev: 0.005225695737503513",
+            "extra": "mean: 96.74580172728818 msec\nrounds: 11"
+          },
+          {
+            "name": "tests/test_filter.py::test_multi_step",
+            "value": 10.37399055495931,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0038455893221927494",
+            "extra": "mean: 96.39492099999529 msec\nrounds: 10"
+          },
+          {
+            "name": "tests/test_filter.py::test_charge",
+            "value": 10.511943769418943,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0036371422955658135",
+            "extra": "mean: 95.12988481817915 msec\nrounds: 11"
+          },
+          {
+            "name": "tests/test_filter.py::test_discharge",
+            "value": 10.382787758028579,
+            "unit": "iter/sec",
+            "range": "stddev: 0.004394745920276508",
+            "extra": "mean: 96.31324681820078 msec\nrounds: 11"
+          },
+          {
+            "name": "tests/test_filter.py::test_chargeordischarge",
+            "value": 9.941030848811923,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0034211273225577556",
+            "extra": "mean: 100.5931895000117 msec\nrounds: 10"
+          },
+          {
+            "name": "tests/test_filter.py::test_rest",
+            "value": 10.710876905029416,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00353539248872688",
+            "extra": "mean: 93.36303730000282 msec\nrounds: 10"
+          },
+          {
+            "name": "tests/test_filter.py::test_negative_cycle_index",
+            "value": 10.78663836238309,
+            "unit": "iter/sec",
+            "range": "stddev: 0.002302401530908723",
+            "extra": "mean: 92.70728900000594 msec\nrounds: 11"
+          },
+          {
+            "name": "tests/test_filter.py::test_negative_step_index",
+            "value": 10.066135682767829,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0034469623070816436",
+            "extra": "mean: 99.34298836363743 msec\nrounds: 11"
+          },
+          {
+            "name": "tests/test_filter.py::test_cycle",
+            "value": 10.6035183977979,
+            "unit": "iter/sec",
+            "range": "stddev: 0.004559404223528857",
+            "extra": "mean: 94.30831941666422 msec\nrounds: 12"
+          },
+          {
+            "name": "tests/test_filter.py::test_all_steps",
+            "value": 9.689242475381121,
+            "unit": "iter/sec",
+            "range": "stddev: 0.005080740904116822",
+            "extra": "mean: 103.20724272726652 msec\nrounds: 11"
+          },
+          {
+            "name": "tests/test_procedure.py::test_experiment",
+            "value": 20320.36262934205,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000020752345895478486",
+            "extra": "mean: 49.2117201961754 usec\nrounds: 4882"
+          },
+          {
+            "name": "tests/test_procedure.py::test_process_readme",
+            "value": 548.8865132258064,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00011683804888641206",
+            "extra": "mean: 1.8218702334713952 msec\nrounds: 484"
+          },
+          {
+            "name": "tests/test_rawdata.py::test_set_SOC",
+            "value": 1555.4756025167737,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002616504370522655",
+            "extra": "mean: 642.8901863725737 usec\nrounds: 499"
           }
         ]
       }
