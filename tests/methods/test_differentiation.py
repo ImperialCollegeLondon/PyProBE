ccb14a80e5bf2e678ad36fd23f0e5242db3d8b5d
Tom Holland <thomas.holland7@outlook.com>
2024-07-18 17:31:43 +0100

    Refactor differentiation methods into function form

--- tests/methods/test_differentiation.py
+++ tests/methods/test_differentiation.py
@@ -1,11 +1,11 @@
-"""Tests for the LEAN differentiation method."""
+"""Tests for the differentiation module."""
 
 import numpy as np
 
-from pyprobe.methods.differentiation.LEAN import DifferentiateLEAN
+import pyprobe.methods.differentiation as diff
 
 
-def test_differentiate():
+def test_differentiate_LEAN():
     """Test the LEAN differentiation method."""
     x_min = 0
     x_max = 1
@@ -25,16 +25,16 @@ def test_differentiate():
     if remainder > 0:
         y = np.append(y, levels[:remainder])
 
-    x_pts, _, dxdy = DifferentiateLEAN.gradient(x, y, 1, "dxdy")
+    x_pts, _, dxdy = diff._calc_gradient_with_LEAN(x, y, 1, "dxdy")
     assert np.isclose(np.median(dxdy), 1 / average_gradient, rtol=0.1)
-    x_pts, _, dydx = DifferentiateLEAN.gradient(x, y, 1, "dydx")
+    x_pts, _, dydx = diff._calc_gradient_with_LEAN(x, y, 1, "dydx")
     assert np.isclose(np.median(dydx), average_gradient, rtol=0.1)
 
 
 def test_x_sections():
     """Test the LEAN get_x_sections method."""
     x = [0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 14, 16, 18, 20, 20.5, 21, 21.5, 22, 22.5, 23]
-    x_sections = DifferentiateLEAN.get_x_sections(x)
+    x_sections = diff._get_x_sections(x)
     x_sliced = [x[s] for s in x_sections]
     assert x_sliced == [
         [0, 1, 2, 3, 4, 5, 6],
