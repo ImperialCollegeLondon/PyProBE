782d1f3ff5f746af2868f2480afbd930c5b1503c
Tom Holland <thomas.holland7@outlook.com>
2024-07-23 14:26:08 +0100

    Remove unnecessary tests in utils module

---
+++ tests/analysis/test_utils.py
@@ -0,0 +1,20 @@
+"""Tests for the BaseMethod class."""
+
+import numpy as np
+import polars as pl
+import pytest
+
+import pyprobe.analysis.utils as utils
+from pyprobe.result import Result
+
+
+@pytest.fixture
+def input_data_fixture():
+    """Return a Result instance."""
+    return Result(_data=pl.LazyFrame({"x": [1, 2, 3]}), info={})
+
+
+def test_assemble_array(input_data_fixture):
+    """Test the assemble array method."""
+    array = utils.assemble_array([input_data_fixture, input_data_fixture], "x")
+    assert np.array_equal(array, np.array([[1, 2, 3], [1, 2, 3]]))
