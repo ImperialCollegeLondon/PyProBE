5d553f1bfbede3c57215b258785b3121fbb21ada
Tom Holland <thomas.holland7@outlook.com>
2024-07-29 13:47:54 +0100

    Add docstrings for typing module

--- pyprobe/typing.py
+++ pyprobe/typing.py
@@ -7,9 +7,12 @@ if TYPE_CHECKING:
     from pyprobe.result import Result
 
 FilterToExperimentType = Union["Procedure", "Experiment"]
+"""Type alias for filtering to an experiment."""
 FilterToCycleType = Union["Procedure", "Experiment", "Cycle"]
+"""Type alias for filtering to a cycle."""
 FilterToStepType = Union["Procedure", "Experiment", "Cycle", "Step"]
-
+"""Type alias for filtering to a step."""
 PyProBERawDataType = Union["RawData", FilterToStepType]
-
+"""Type alias for raw data in PyProbe."""
 PyProBEDataType = Union[PyProBERawDataType, "Result"]
+"""Type alias for data in PyProbe."""
