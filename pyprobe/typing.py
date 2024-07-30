1f4b0edb020b94b422fa8cea28c2f6c38c2b4bb3
Tom Holland <thomas.holland7@outlook.com>
2024-07-30 18:05:06 +0100

    Remove cerberus typing

--- pyprobe/typing.py
+++ pyprobe/typing.py
@@ -1,9 +1,6 @@
 """Module for type hints and type aliases."""
-from typing import Any, Union
+from typing import Union
 
-import cerberus
-
-# if TYPE_CHECKING:
 from pyprobe.filters import Cycle, Experiment, Procedure, Step
 from pyprobe.rawdata import RawData
 from pyprobe.result import Result
@@ -18,40 +15,3 @@ PyProBERawDataType = Union[RawData, FilterToStepType]
 """Type alias for raw data in PyProbe."""
 PyProBEDataType = Union[PyProBERawDataType, Result]
 """Type alias for data in PyProbe."""
-
-
-class PyProBEValidator(cerberus.Validator):
-    """A custom validator for PyProBE data types."""
-
-    types_mapping = cerberus.Validator.types_mapping.copy()
-    types_mapping["Experiment"] = cerberus.TypeDefinition(
-        "Experiment", (Experiment,), ()
-    )
-    types_mapping["Procedure"] = cerberus.TypeDefinition("Procedure", (Procedure,), ())
-    types_mapping["Cycle"] = cerberus.TypeDefinition("Cycle", (Cycle,), ())
-    types_mapping["Step"] = cerberus.TypeDefinition("Step", (Step,), ())
-    types_mapping["RawData"] = cerberus.TypeDefinition("RawData", (RawData,), ())
-    types_mapping["Result"] = cerberus.TypeDefinition("Result", (Result,), ())
-    types_mapping["FilterToExperimentType"] = cerberus.TypeDefinition(
-        "FilterToExperimentType", (FilterToExperimentType,), ()
-    )
-    types_mapping["FilterToCycleType"] = cerberus.TypeDefinition(
-        "FilterToCycleType", (FilterToCycleType,), ()
-    )
-    types_mapping["FilterToStepType"] = cerberus.TypeDefinition(
-        "FilterToStepType", (FilterToStepType,), ()
-    )
-    types_mapping["PyProBERawDataType"] = cerberus.TypeDefinition(
-        "PyProBERawDataType", (PyProBERawDataType,), ()
-    )
-    types_mapping["PyProBEDataType"] = cerberus.TypeDefinition(
-        "PyProBEDataType", (PyProBEDataType,), ()
-    )
-
-    def _validate_contains_columns(self, columns: Any, field: Any, value: Any) -> None:
-        """Validate that the value contains the specified column."""
-        if isinstance(columns, str):
-            column = [columns]
-        for column in columns:
-            if column not in value.column_list:
-                self._error(field, f"Column '{column}' not in data.")
