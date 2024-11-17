db76030e3a8125ae71bb95e6b655f4ff59d4e931
Tom Holland <thomas.holland7@outlook.com>
2024-11-17 16:35:45 +0000

    Move differentiation methods out of class

--- pyprobe/analysis/differentiation.py
+++ pyprobe/analysis/differentiation.py
@@ -4,12 +4,150 @@ from typing import List
 
 import numpy as np
 import polars as pl
+from deprecated import deprecated
 from pydantic import BaseModel
 
 import pyprobe.analysis.base.differentiation_functions as diff_functions
 from pyprobe.analysis.utils import AnalysisValidator
 from pyprobe.result import Result
 from pyprobe.typing import PyProBEDataType
+
+
+def gradient(  # 1. Define the method
+    input_data: PyProBEDataType,
+    x: str,
+    y: str,
+) -> Result:
+    """Differentiate smooth data with a finite difference method.
+
+    A wrapper of the numpy.gradient function. This method calculates the gradient
+    of the data in the y column with respect to the data in the x column.
+
+    Args:
+        x (str): The name of the x variable.
+        y (str): The name of the y variable.
+
+    Returns:
+        Result:
+            A result object containing the columns, `x`, `y` and the
+            calculated gradient.
+    """
+    # 2. Validate the inputs to the method
+    validator = AnalysisValidator(
+        input_data=input_data,
+        required_columns=[x, y],
+        # required_type not neccessary here as type specified when declaring
+        # input_data attribute is strict enough
+    )
+    # 3. Retrieve the validated columns as numpy arrays
+    x_data, y_data = validator.variables
+
+    # 4. Perform the computation
+    gradient_title = f"d({y})/d({x})"
+    gradient_data = np.gradient(y_data, x_data)
+
+    # 5. Create a Result object to store the results
+    gradient_result = input_data.clean_copy(
+        pl.DataFrame({x: x_data, y: y_data, gradient_title: gradient_data})
+    )
+    # 6. Define the column definitions for the Result object
+    gradient_result.column_definitions = {
+        x: input_data.column_definitions[x],
+        y: input_data.column_definitions[y],
+        gradient_title: "The calculated gradient.",
+    }
+    # 7. Return the Result object
+    return gradient_result
+
+
+def differentiate_LEAN(
+    input_data: PyProBEDataType,
+    x: str,
+    y: str,
+    k: int = 1,
+    gradient: str = "dydx",
+    smoothing_filter: List[float] = [0.0668, 0.2417, 0.3830, 0.2417, 0.0668],
+    section: str = "longest",
+) -> Result:
+    r"""A method for differentiating noisy data.
+
+    Uses 'Level Evaluation ANalysis' (LEAN) method described in the paper of
+    :footcite:t:`Feng2020`.
+
+    This method assumes :math:`x` datapoints to be evenly spaced, it can return
+    either :math:`\frac{dy}{dx}` or :math:`\frac{dx}{dy}` depending on the argument
+    provided to the `gradient` parameter.
+
+    Args:
+        x (str):
+            The name of the x variable.
+        y (str):
+            The name of the y variable.
+        k (int, optional):
+            The integer multiple to apply to the sampling interval for the bin size
+            (:math:`\delta R` in paper). Default is 1.
+        gradient (str, optional):
+            The gradient to calculate, either 'dydx' or 'dxdy'. Default is 'dydx'.
+        smoothing_filter (List[float], optional):
+            The coefficients of the smoothing matrix.
+
+            Examples provided by :footcite:t:`Feng2020` include:
+
+                - [0.25, 0.5, 0.25] for a 3-point smoothing filter.
+                - [0.0668, 0.2417, 0.3830, 0.2417, 0.0668] (default) for a 5-point
+                    smoothing filter.
+                - [0.1059, 0.121, 0.1745, 0.1972, 0.1745, 0.121, 0.1059] for a
+                    7-point smoothing filter.
+
+        section (str, optional):
+            The section of the data with constant sample rate in x to be considered.
+            Default is 'longest', which just returns the longest unifomly sampled
+            section. Alternative is 'all', which returns all sections.
+
+    Returns:
+        Result:
+            A result object containing the columns, `x`, `y` and the calculated
+            gradient.
+    """
+    # validate and identify variables
+    validator = AnalysisValidator(input_data=input_data, required_columns=[x, y])
+    x_data, y_data = validator.variables
+
+    # split input data into uniformly sampled sections
+    x_sections = diff_functions.get_x_sections(x_data)
+    if section == "longest":
+        x_sections = [max(x_sections, key=lambda x: x.stop - x.start)]
+    x_all = np.array([])
+    y_all = np.array([])
+    calc_gradient_all = np.array([])
+
+    # over each uniformly sampled section, calculate the gradient
+    for i in range(len(x_sections)):
+        x_data = x_data[x_sections[i]]
+        y_data = y_data[x_sections[i]]
+        x_pts, y_pts, calculated_gradient = diff_functions.calc_gradient_with_LEAN(
+            x_data, y_data, k, gradient
+        )
+        x_all = np.append(x_all, x_pts)
+        y_all = np.append(y_all, y_pts)
+        calc_gradient_all = np.append(calc_gradient_all, calculated_gradient)
+
+    # smooth the calculated gradient
+    smoothed_gradient = diff_functions.smooth_gradient(
+        calc_gradient_all, smoothing_filter
+    )
+
+    # output the results
+    gradient_title = f"d({y})/d({x})" if gradient == "dydx" else f"d({x})/d({y})"
+    gradient_result = input_data.clean_copy(
+        pl.DataFrame({x: x_all, y: y_all, gradient_title: smoothed_gradient})
+    )
+    gradient_result.column_definitions = {
+        x: input_data.column_definitions[x],
+        y: input_data.column_definitions[y],
+        gradient_title: "The calculated gradient.",
+    }
+    return gradient_result
 
 
 # 1. Define the class as a Pydantic BaseModel.
@@ -20,6 +158,7 @@ class Differentiation(BaseModel):
     input_data: PyProBEDataType
     """The input data for the differentiation."""
 
+    @deprecated("Use differentiation.gradient instead.", version="1.1.0")
     def differentiate_FD(  # 1. Define the method
         self,
         x: str,
@@ -75,6 +214,7 @@ class Differentiation(BaseModel):
         # 7. Return the Result object
         return gradient_result
 
+    @deprecated("Use differentiation.differentiate_LEAN instead.", version="1.1.0")
     def differentiate_LEAN(
         self,
         x: str,
--- tests/analysis/test_differentiation.py
+++ tests/analysis/test_differentiation.py
@@ -4,7 +4,7 @@ import numpy as np
 import polars as pl
 import pytest
 
-from pyprobe.analysis.differentiation import Differentiation
+from pyprobe.analysis import differentiation
 from pyprobe.result import Result
 
 x_data = np.array([1, 2, 3, 4, 5])
@@ -18,33 +18,21 @@ def differentiation_fixture():
         base_dataframe=pl.DataFrame({"x": x_data, "y": y_data}), info={}
     )
     input_data.column_definitions = {"x": "The x data", "y": "The y data"}
-    return Differentiation(input_data=input_data)
+    return input_data
 
 
-def test_differentiate_FD(differentiation_fixture):
+def test_gradient(differentiation_fixture):
     """Test the finite difference differentiation method."""
-    # Test case 1: dydx gradient
-    result = differentiation_fixture.differentiate_FD("x", "y", gradient="dydx")
+    result = differentiation.gradient(differentiation_fixture, "x", "y")
     expected_gradient = np.array([2, 2, 2, 2, 2])
     assert isinstance(result, Result)
     assert np.allclose(result.get_only("d(y)/d(x)"), expected_gradient)
-
-    # Test case 2: dxdy gradient
-    result = differentiation_fixture.differentiate_FD("x", "y", gradient="dxdy")
-    expected_gradient = np.array([0.5, 0.5, 0.5, 0.5, 0.5])
-    assert isinstance(result, Result)
-    assert np.allclose(result.get_only("d(x)/d(y)"), expected_gradient)
-
-    # Test case 3: Invalid gradient option
-    try:
-        result = differentiation_fixture.differentiate_FD("x", "y", gradient="invalid")
-        assert False  # Should raise a ValueError
-    except ValueError:
-        assert True
 
 
 def test_differentiate_LEAN(differentiation_fixture):
     """Test the LEAN differentiation method."""
     # Test case 1: dydx gradient
-    result = differentiation_fixture.differentiate_LEAN("x", "y", gradient="dydx")
+    result = differentiation.differentiate_LEAN(
+        differentiation_fixture, "x", "y", gradient="dydx"
+    )
     assert isinstance(result, Result)
