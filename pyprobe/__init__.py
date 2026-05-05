0f81aa9d58b709c68ac04870b3885baac5ac0af9
Tom Holland <137503955+tomjholland@users.noreply.github.com>
2026-05-05 10:50:47 +0100

    feat: make Procedure class importable from root

--- pyprobe/__init__.py
+++ pyprobe/__init__.py
@@ -5,6 +5,7 @@ from loguru import logger  # noqa: F401
 from ._version import __version__  # noqa: F401
 from .cell import Cell, load_archive, make_cell_list  # noqa: F401
 from .dashboard import launch_dashboard  # noqa: F401
+from .filters import Procedure  # noqa: F401
 from .result import Result  # noqa: F401
 from .utils import set_log_level
 
